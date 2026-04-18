355581e0a9ce47ce79d97f92eb13821a8a675a03
venghour-holberton-git <13067@holbertonstudents.com>
2026-04-18 10:54:31 +0000

    print all arguments

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include "simple_shell.h"
 
-int main(void)
+int main(int ac, char ** av)
 {
+	int i = 0;
+
+	while (av[i] != NULL)
+	{
+		printf("parameter at index %d is %s\n", i, av[i]);
+		i++;
+	}
 	return (0);
 }
