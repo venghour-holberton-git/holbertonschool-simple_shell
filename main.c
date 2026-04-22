29493135222654f4eee744cf6a1503412e75c100
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-22 14:22:25 +0000

    handle command lines with arguments

--- main.c
+++ main.c
@@ -6,8 +6,11 @@ int main(int ac, char **av, char **env)
 	size_t size = 0;
 	ssize_t nread;
 	pid_t pid;
-	int w_status;
+	int w_status, index;
+	char **command_inputs;
+	char *argv[100];
 
+	index = 0;
 	printf("$ ");
 	while ((nread = getline(&line, &size, stdin)) != -1)
 	{
@@ -17,9 +20,17 @@ int main(int ac, char **av, char **env)
 		if (pid == 0)
 		{
 			line[strcspn(line, "\n")] = '\0';
-			char *argv[] = {line, NULL};
+			command_inputs = string_to_array(line);
 
-			if (execve(line, argv, NULL) == -1)
+			while (command_inputs[index] != NULL)
+			{
+				printf("hh %s", command_inputs[index]);
+				argv[index] = command_inputs[index];
+				index++;
+			}
+			argv[index] = NULL;
+
+			if (execve(command_inputs[0], argv, NULL) == -1)
             		{
                 		perror("Error");
                			 exit(1);
