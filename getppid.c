3e61754f310b30496c2e381f3ca2d9c433dc1ae6
lluchetti22 <lachlan.luchetti@outlook.com>
2026-04-22 21:41:41 +1000

    Update getppid.c

--- getppid.c
+++ getppid.c
@@ -3,8 +3,8 @@
 int main(void)
 {
 	pid_t my_pid, my_ppid;
-	my_pid = getpid();
 
+	my_pid = getpid();
 	my_ppid = getppid();
 	printf("my pid is %d\n -- and my ppid is %d\n", my_pid, my_ppid);
 	return (0);
