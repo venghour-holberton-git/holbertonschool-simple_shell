91639f679224f544d69379be7898ef4ef1182b3a
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-22 15:13:26 +0000

    handle path but not fork condition yet

--- main.c
+++ main.c
@@ -6,31 +6,47 @@ int main(int ac, char **av, char **env)
 	size_t size = 0;
 	ssize_t nread;
 	pid_t pid;
-	int w_status, index;
+	int w_status, index = 0;
 	char **command_inputs;
 	char *argv[100];
+	char full_path[1024];
 
-	index = 0;
 	printf("$ ");
 	while ((nread = getline(&line, &size, stdin)) != -1)
 	{
 		printf("%s", line);
 		pid = fork();
+		index = 0;
 		
 		if (pid == 0)
 		{
+			char *path = strdup(getenv("PATH"));
+			char *dir;
+
 			line[strcspn(line, "\n")] = '\0';
 			command_inputs = string_to_array(line);
+			dir = strtok(path, ":");
+
+			while(dir != NULL)
+			{
+				sprintf(full_path, "%s/%s", dir, command_inputs[0]);
+				if (access(full_path, X_OK) == 0)
+				{
+					argv[0] = full_path;
+					index++;
+					break;
+				}
+				dir = strtok(NULL, ":");
+			}
 
 			while (command_inputs[index] != NULL)
 			{
-				printf("hh %s", command_inputs[index]);
 				argv[index] = command_inputs[index];
 				index++;
 			}
 			argv[index] = NULL;
 
-			if (execve(command_inputs[0], argv, NULL) == -1)
+			if (execve(argv[0], argv, NULL) == -1)
             		{
                 		perror("Error");
                			 exit(1);
