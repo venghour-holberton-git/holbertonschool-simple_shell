b5b490b69d0be021525a8d196c6c42810551c13c
lluchetti22 <lachlan.luchetti@outlook.com>
2026-04-22 21:40:14 +1000

    Change main function signature to 'void'

--- getppid.c
+++ getppid.c
@@ -1,9 +1,10 @@
 #include "simple_shell.h"
 
-int main()
+int main(void)
 {
 	pid_t my_pid, my_ppid;
 	my_pid = getpid();
+
 	my_ppid = getppid();
 	printf("my pid is %d\n -- and my ppid is %d\n", my_pid, my_ppid);
 	return (0);
