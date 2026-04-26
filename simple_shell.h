7ccbf8c9796fe2ac0a3ce026bb3d404eac93e3d7
lluchetti22 <lachlan.luchetti@outlook.com>
2026-04-26 14:21:49 +1000

    Update simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -33,7 +33,7 @@ char *search_for_path(char *short_path, int *is_founded);
 char *get_available_path(char *user_command, int *is_founded);
 int exec_child_command(char *user_command, char *file_path);
 char **get_argv_from_command(char *user_command, char *file_path);
-void handle_parent_child_action(char *line, char *full_path);
+int handle_parent_child_action(char *line, char *full_path);
 int handle_print_env_and_exit(char *line);
 
 #endif
