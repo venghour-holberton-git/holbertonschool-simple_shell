1cbb1d3a1226b7294205db12a6eb0b423187b703
lluchetti22 <lachlan.luchetti@outlook.com>
2026-04-26 14:18:44 +1000

    Update main.c

--- main.c
+++ main.c
@@ -5,35 +5,68 @@
  * Return: 0
  */
 
+#include "simple_shell.h"
+
+/**
+ * main - entry point of the program
+ * Return: 0
+ */
 int main(int argc, char **args)
 {
-	char *line = NULL;
-	size_t size = 0;
-	ssize_t nread;
-	int found = 0;
-	char *full_path = NULL;
-	(void)argc;
+    char *line = NULL;
+    size_t size = 0;
+    ssize_t nread;
+    int found = 0;
+    char *full_path = NULL;
+    char **parsed_args = NULL;
+    (void)argc;
+    (void)args;
 
-	while (1)
-	{
-		if (isatty(STDIN_FILENO))
-			printf("$ ");
-		nread = getline(&line, &size, stdin);
-		if (nread == -1)
-			break;		
-		found = 0;
-		if (handle_print_env_and_exit(line) == 0)
-			continue;
-		full_path = get_available_path(line, &found);
-		if (found == 0)
-		{
-			perror("Error");
-			free(full_path);
-			continue;
-		}
-		handle_parent_child_action(line, full_path);
-		free(full_path);
-	}
-	free(line);
-	return (0);
+    while (1)
+    {
+        if (isatty(STDIN_FILENO))
+            printf("$ ");
+
+        nread = getline(&line, &size, stdin);
+        if (nread == -1)
+            break;
+
+        line[strcspn(line, "\n")] = '\0';
+
+        if (line[0] == '\0')
+            continue;
+
+        parsed_args = string_to_array(line);
+        if (parsed_args == NULL || parsed_args[0] == NULL)
+        {
+            free(parsed_args);
+            continue;
+        }
+
+        if (strcmp(parsed_args[0], "exit") == 0)
+        {
+            user_exit(parsed_args, line);
+        }
+        if (strcmp(parsed_args[0], "env") == 0)
+        {
+            print_env();
+            free(parsed_args);
+            continue;
+        }
+
+        free(parsed_args);
+
+        found = 0;
+        full_path = get_available_path(line, &found);
+        if (found == 0)
+        {
+            fprintf(stderr, "./hsh: 1: %s: not found\n", line);
+            free(full_path);
+            continue;
+        }
+        handle_parent_child_action(line, full_path);
+        free(full_path);
+    }
+    free(line);
+    return (0);
 }
